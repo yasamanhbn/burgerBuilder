923b350b19b2f14aa80f891983287eac420bd3ea
yasaman <hbn.yasaman@gmail.com>
2020-10-02 17:25:10 +0330

    add build files

---
+++ build/precache-manifest.f00fb72bf19cde5637025d329239db85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea74ab64226b25da5332315ad96e27c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c7ab7baacfe85c00d73",
+    "url": "/static/css/main.c762caf0.chunk.css"
+  },
+  {
+    "revision": "c9d626ffe4b995e82c2b",
+    "url": "/static/js/2.abca3877.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.abca3877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c7ab7baacfe85c00d73",
+    "url": "/static/js/main.db4e314d.chunk.js"
+  },
+  {
+    "revision": "e1e5c78d49d83331038d",
+    "url": "/static/js/runtime-main.6ea168cc.js"
+  }
+]);
