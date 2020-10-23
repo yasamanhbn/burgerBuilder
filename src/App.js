cbc68b4ac7fa4626de6c09272156f51dd102f607
yasamanhbn <hbn.yasaman@gmail.com>
2020-10-23 23:05:37 +0330

    add an alert for turning on vpn

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Layout from './Container/layout'
 const App = ()=> {
+    alert("please turn on your vpn");
   return (
     <div className="App">
       <Layout/>
